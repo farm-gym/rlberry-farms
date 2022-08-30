10e3795963bea183ea19647ec60e08e013072c61
TMat <timothee.mathieu@inria.fr>
2022-08-30 14:17:44 +0200

    farm1

--- rlberry_farms/farm1/farm.py
+++ rlberry_farms/farm1/farm.py
@@ -19,74 +19,143 @@ from farmgym.v2.entities.Cide import Cide
 from farmgym.v2.rendering.monitoring import mat2d_value, sum_value
 
 
-from gym.envs.registration import  register
+from gym.envs.registration import register
 
 import os
 from pathlib import Path
+
 file_path = Path(os.path.realpath(__file__))
 CURRENT_DIR = file_path.parent
-
 
 
 def env():
     ##########################################################################
     entities1 = []
-    entities1.append((Weather,"montpellier"))
-    entities1.append((Soil,"clay"))
-    entities1.append((Plant, 'bean'))
+    entities1.append((Weather, "montpellier"))
+    entities1.append((Soil, "clay"))
+    entities1.append((Plant, "bean"))
 
+    entities1.append((Birds, "base_bird"))
+    entities1.append((Facility, "base_facility"))
 
-    entities1.append((Birds,"base_bird"))
-    entities1.append((Facility,"base_facility"))
+    entities1.append((Fertilizer, "basic_N"))
 
-    entities1.append((Fertilizer,"basic_N"))
+    entities1.append((Pests, "basic"))
+    entities1.append((Pollinators, "bee"))
+    entities1.append((Cide, "pesticide"))
 
-    entities1.append((Pests,"basic"))
-    entities1.append((Pollinators,"bee"))
-    entities1.append((Cide,"pesticide"))
+    entities1.append((Weeds, "base_weed"))
+    entities1.append((Cide, "herbicide"))
 
-    entities1.append((Weeds,"base_weed"))
-    entities1.append((Cide,"herbicide"))
-
-    field1 = Field(localization={'latitude#°':43, 'longitude#°':4, 'altitude#m':150},
-                  shape={'length#nb':1, 'width#nb':1, 'scale#m': 1.}, entity_managers=entities1)
+    field1 = Field(
+        localization={"latitude#°": 43, "longitude#°": 4, "altitude#m": 150},
+        shape={"length#nb": 1, "width#nb": 1, "scale#m": 1.0},
+        entity_managers=entities1,
+    )
 
     farmer1 = BasicFarmer(max_daily_interventions=1)
-    scoring = BasicScore(score_configuration= CURRENT_DIR / 'farm_score.yaml')
+    scoring = BasicScore(score_configuration=CURRENT_DIR / "farm_score.yaml")
 
     free_observations = []
-    free_observations.append( ("Field-0", "Weather-0", "day#int365", []))
+    free_observations.append(("Field-0", "Weather-0", "day#int365", []))
     free_observations.append(("Field-0", "Weather-0", "air_temperature", []))
     free_observations.append(("Field-0", "Weather-0", "rain_amount", []))
     free_observations.append(("Field-0", "Weather-0", "sun_exposure#int5", []))
     free_observations.append(("Field-0", "Weather-0", "consecutive_dry#day", []))
-    
+
     free_observations.append(("Field-0", "Plant-0", "stage", []))
     free_observations.append(("Field-0", "Plant-0", "size#cm", []))
+    free_observations.append(("Field-0", "Soil-0", "wet_surface#m2.day-1", []))
+    free_observations.append(("Field-0", "Soil-0", "microlife_health_index#%", []))
 
+    free_observations.append(("Field-0", "Birds-0", "population#nb", []))
+    free_observations.append(("Field-0", "Fertilizer-0", "amount#kg", []))
+    free_observations.append(("Field-0", "Pests-0", "plot_population#nb", []))
+    free_observations.append(("Field-0", "Pollinators-0", "occurrence#bin", []))
+    free_observations.append(("Field-0", "Weeds-0", "grow#nb", []))
+    free_observations.append(("Field-0", "Weeds-0", "flowers#nb", []))
 
-    
-    terminal_CNF_conditions = [[(("Field-0", "Weather-0", "day#int365", []), lambda x: x.value, ">=", 360)], [(("Field-0","Plant-0","global_stage",[]),lambda x:x.value,"==","dead")]]
-    rules = BasicRule(init_configuration=CURRENT_DIR/'farm_init.yaml',
-                      actions_configuration=CURRENT_DIR/'farm_actions.yaml',
-                      terminal_CNF_conditions=terminal_CNF_conditions,
-                      free_observations=free_observations)
+    terminal_CNF_conditions = [
+        [(("Field-0", "Weather-0", "day#int365", []), lambda x: x.value, ">=", 360)],
+        [(("Field-0", "Plant-0", "global_stage", []), lambda x: x.value, "==", "dead")],
+    ]
+    rules = BasicRule(
+        init_configuration=CURRENT_DIR / "farm_init.yaml",
+        actions_configuration=CURRENT_DIR / "farm_actions.yaml",
+        terminal_CNF_conditions=terminal_CNF_conditions,
+        free_observations=free_observations,
+    )
 
     farm = Farm(fields=[field1], farmers=[farmer1], scoring=scoring, rules=rules)
 
     ##########################################################################
 
     var = []
-    var.append(("Field-0", "Soil-0", "available_N#g", lambda x:sum_value(x), "Available Nitrogen (g)", 'range_auto'))
-    var.append(("Field-0", "Soil-0", "available_Water#L", lambda x:sum_value(x), "Available Water (g)", 'range_auto'))
-    var.append(("Field-0", "Plant-0", "size#cm", lambda x:sum_value(x), "Size (cm)", 'range_auto'))
-    var.append(("Field-0", "Plant-0", "flowers_per_plant#nb", lambda x:sum_value(x), "Flowers (nb)", 'range_auto'))
-    var.append(("Field-0", "Plant-0", "flowers_pollinated_per_plant#nb", lambda x:sum_value(x), "Flowers pollinated (nb)", 'range_auto'))
-    var.append(("Field-0", "Plant-0", "fruits_per_plant#nb", lambda x:sum_value(x), "Fruits (nb)", 'range_auto'))
+    var.append(
+        (
+            "Field-0",
+            "Soil-0",
+            "available_N#g",
+            lambda x: sum_value(x),
+            "Available Nitrogen (g)",
+            "range_auto",
+        )
+    )
+    var.append(
+        (
+            "Field-0",
+            "Soil-0",
+            "available_Water#L",
+            lambda x: sum_value(x),
+            "Available Water (g)",
+            "range_auto",
+        )
+    )
+    var.append(
+        (
+            "Field-0",
+            "Plant-0",
+            "size#cm",
+            lambda x: sum_value(x),
+            "Size (cm)",
+            "range_auto",
+        )
+    )
+    var.append(
+        (
+            "Field-0",
+            "Plant-0",
+            "flowers_per_plant#nb",
+            lambda x: sum_value(x),
+            "Flowers (nb)",
+            "range_auto",
+        )
+    )
+    var.append(
+        (
+            "Field-0",
+            "Plant-0",
+            "flowers_pollinated_per_plant#nb",
+            lambda x: sum_value(x),
+            "Flowers pollinated (nb)",
+            "range_auto",
+        )
+    )
+    var.append(
+        (
+            "Field-0",
+            "Plant-0",
+            "fruits_per_plant#nb",
+            lambda x: sum_value(x),
+            "Fruits (nb)",
+            "range_auto",
+        )
+    )
     farm.add_monitoring(var)
     return farm
 
 
 if __name__ == "__main__":
     from farmgym.v2.games.rungame import run
-    run(env(),max_steps=100,render=False, monitoring=True)
+
+    run(env(), max_steps=100, render=False, monitoring=True)
--- rlberry_farms/game1_env.py
+++ rlberry_farms/game1_env.py
@@ -6,16 +6,16 @@ import numpy as np
 
 class Farm1(Model):
     """
-    TODO : 
-    Farm1 is a very basic 1x1 farm with only one possible plant : beans, planted in a clay ground. The only possible actions are to water the field or to harvest it.
+    TODO :
+    Farm1 is a difficult 1x1 farm with only one possible plant : beans, planted in a clay ground.
     The observation of the field state is free.
     The advised maximum episode length is 365 (as in 365 days).
 
     The Farm has the weather of Montpellier in France (e.g. fairly warm weather, well suited for the culture of beans), the initial day is 120. Initially the field is healthy and contains all the nutrient necessary to the plant.
 
-    The reward is the number of grams of harvested beans.
+    The reward is the number of grams of harvested beans, and there is a high negative reward for very low microlife in soil (due to pesticides).
 
-    The condition for end of episode (self.step returns done) is that the day is >= 365 or that the field has been harvested, or that the plant is dead.
+    The condition for end of episode (self.step returns done) is that the day is >= 365 or that the plant is dead.
 
     Notes
     -----
@@ -30,11 +30,20 @@ class Farm1(Model):
         - consecutive dry day (int)
         - stage of growth of the plant (int)
         - size of the plant in cm.
-    
+        - Soil wet_surface#m2.day-1
+        - Birds population#nb
+        - fertilizer amount#kg
+        - Pests plot_population#nb
+        - Pollinators occurrence#bin
+        - Weeds grow#nb
+        - Weeds flowers#nb
+
     Actions:
         The action is either watering the field with 1L to 5L of water, harvesting or doing nothing.
     """
+
     name = "Farm0"
+
     def __init__(self):
         # init base classes
         Model.__init__(self)
@@ -42,11 +51,14 @@ class Farm1(Model):
         self.farm = cb.env()
         self.farm.monitor = None
         # observation and action spaces
-        # Day, temp mean, temp min, temp max, rain amount, sun exposure, consecutive dry day, stage, size#cm
-        high = np.array([365, 50, 50, 50, 300, 5, 100, 10, 200 ])
-        low =  np.array([0, -50, -50, -50, 0,   0, 0, 0, 0])
+        # Day, temp mean, temp min, temp max, rain amount, sun exposure, consecutive dry day, stage, size#cm, wet surface, microlife %,
+        # bird pop, fertilizer amount, pests pop, pollinators occurrence, weeds grow nb, weeds flower nb
+        high = np.array(
+            [365, 50, 50, 50, 300, 5, 100, 10, 200, 1, 100, 20, 10, 1000, 1, 100, 100]
+        )
+        low = np.array([0, -50, -50, -50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         self.observation_space = spaces.Box(low=low, high=high)
-        self.action_space = spaces.Discrete(8)
+        self.action_space = spaces.Discrete(13)
 
         # initialize
         self.state = None
@@ -59,33 +71,93 @@ class Farm1(Model):
     def step(self, action):
         obs1, _, _, info = self.farm.farmgym_step([])
         obs, reward, is_done, info = self.farm.farmgym_step(self.num_to_action(action))
-        if hasattr(reward, '__len__'):
+        if hasattr(reward, "__len__"):
             reward = reward[0]
-        #reward = reward - info['intervention cost']/10
-        
-        return self.farmgymobs_to_obs([obs1[i][5] for i in range(len(obs1))]), reward, is_done, info
+        if obs1[8][5][0][0][0] < 20:
+            reward -= 300  # if microlife is < 20%, negative reward
+        if obs1[8][5][0][0][0] < 10:
+            reward -= 300  # if microlife is < 10%, negative reward
+
+        observation = self.farmgymobs_to_obs([obs1[i][5] for i in range(len(obs1))])
+        return observation, reward, is_done, info
 
     def farmgymobs_to_obs(self, obs):
-        return np.array([ obs[0],
-                          obs[1]['mean#°C'][0],
-                          obs[1]['min#°C'][0],
-                          obs[1]['max#°C'][0],
-                          obs[2],
-                          obs[3],
-                          obs[4][0],
-                          obs[5][0][0],
-                          obs[6][0][0][0],
-                         ]
-                        )
+        return np.array(
+            [
+                obs[0],
+                obs[1]["mean#°C"][0],
+                obs[1]["min#°C"][0],
+                obs[1]["max#°C"][0],
+            ]
+            + [np.array([obs[i]]).ravel()[0] for i in range(2, 15)]
+        )
 
     def num_to_action(self, num):
-        if (num >= 1) and (num <= 5) :
-            return [('BasicFarmer-0', 'Field-0', 'Soil-0', 'water_discrete', {'plot': (0, 0), 'amount#L': num, 'duration#min': 60})]
+        if (num >= 1) and (num <= 5):
+            return [
+                (
+                    "BasicFarmer-0",
+                    "Field-0",
+                    "Soil-0",
+                    "water_discrete",
+                    {"plot": (0, 0), "amount#L": num, "duration#min": 60},
+                )
+            ]
         elif num == 6:
-            return [('BasicFarmer-0', 'Field-0', 'Plant-0', 'harvest', {})]
+            return [("BasicFarmer-0", "Field-0", "Plant-0", "harvest", {})]
         elif num == 7:
-            return [('BasicFarmer-0', 'Field-0', 'Plant-0', 'sow', {'plot': (0, 0), "amount#seed": 1, "spacing#cm":10})]
+            return [
+                (
+                    "BasicFarmer-0",
+                    "Field-0",
+                    "Plant-0",
+                    "sow",
+                    {"plot": (0, 0), "amount#seed": 1, "spacing#cm": 10},
+                )
+            ]
+        elif num == 8:
+            return [
+                (
+                    "BasicFarmer-0",
+                    "Field-0",
+                    "Facility-0",
+                    "put_scarecrow",
+                    {"type": "basic"},
+                )
+            ]
+        elif num == 9:
+            return [
+                (
+                    "BasicFarmer-0",
+                    "Field-0",
+                    "Facility-0",
+                    "put_scarecrow",
+                    {"type": "advanced"},
+                )
+            ]
+        elif num == 10:
+            return [("BasicFarmer-0", "Field-0", "Facility-0", "remove_scarecrow", {})]
+        elif num == 11:
+            return [
+                (
+                    "BasicFarmer-0",
+                    "Field-0",
+                    "Fertilizer-0",
+                    "scatter_bag",
+                    {"plot": (0, 0), "amount#bag": 1},
+                )
+            ]
+        elif num == 12:
+            return [
+                (
+                    "BasicFarmer-0",
+                    "Field-0",
+                    "Cide-0",
+                    "scatter_bag",
+                    {"plot": (0, 0), "amount#bag": 1},
+                )
+            ]
+        elif num == 13:
+            return [("BasicFarmer-0", "Field-0", "Weeds-0", "remove", {"plot": (0, 0)})]
         else:
-            return [] # Do nothing.
-
-
+            return []  # Do nothing.
