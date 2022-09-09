229ba6e87cae29e97a1303ba00b5f8197500ea7e
JulienT01 <julien.tj@gmail.com>
2022-09-09 10:13:40 +0200

    add comments and use 'black'

--- examples/expert_farm0.py
+++ examples/expert_farm0.py
@@ -6,12 +6,9 @@ Expert agent on Farm0
 from rlberry.agents import AgentWithSimplePolicy
 from rlberry.manager import AgentManager, evaluate_agents, plot_writer_data
 from rlberry_farms import Farm0
-# from rlberry.agents.torch.utils.training import model_factory_from_env
 import numpy as np
 
 env_ctor, env_kwargs = Farm0, {}
-
-
 
 
 class ExpertAgent(AgentWithSimplePolicy):
@@ -25,7 +22,7 @@ class ExpertAgent(AgentWithSimplePolicy):
         episode_reward = 0
         for ep in range(int(budget)):
             action = self.policy(observation)
-            observation, reward, done, _ = self.env.step(action)
+            observation, reward, done, info = self.env.step(action)
             episode_reward += reward
             if done:
                 self.writer.add_scalar("episode_rewards", episode_reward, ep)
@@ -33,21 +30,34 @@ class ExpertAgent(AgentWithSimplePolicy):
                 self.env.reset()
 
     def policy(self, observation):
+        # Policy : if the plants are in 'entered_ripe' or 'ripe' we harvest them, otherwise we use 1L water.
+
         # The actions are :
         #   0: doing nothing.
         #   1-5 : 5 levels of watering the field (from 1L to 5L of water)
         #   6 : harvesting
-        
-        # stage for plants (observation[7]) : [0:'none', 1:'seed', 2:'entered_grow', 3:'grow', 4:'entered_bloom', 5:'bloom', 6:'entered_fruit', 7:'fruit', 8:'entered_ripe', 9:'ripe', 10:'entered_seed', 11:'harvested', 12:'dead']
 
-        # observation_txt = self.env.observations_txt
-        # my_dict_obs = dict(zip(observation_txt,observation))
-                
-        if(observation[7] in [8,9]):
-            next_action = 6 #harvestiong
+        # states for 'stage for plants' (observation[7]) are :
+        # 0:'none'
+        # 1:'seed'
+        # 2:'entered_grow'
+        # 3:'grow'
+        # 4:'entered_bloom'
+        # 5:'bloom'
+        # 6:'entered_fruit'
+        # 7:'fruit'
+        # 8:'entered_ripe'
+        # 9:'ripe'
+        # 10:'entered_seed'
+        # 11:'harvested'
+        # 12:'dead'
+
+        # print(self.env.farm.get_free_observations())
+        if observation[7] in [8, 9]:
+            next_action = 6  # harvesting
         else:
-            next_action = 1 #watering slowly
-        
+            next_action = 1  # watering slowly
+
         return next_action
 
 
@@ -67,6 +77,3 @@ if __name__ == "__main__":
     evaluation = evaluate_agents([manager], n_simulations=128, plot=False).values
     np.savetxt("expert_farm0.out", np.array(evaluation), delimiter=",")
     data = plot_writer_data("expert_results", "episode_rewards", smooth_weight=0.95)
-
-
-# This template file gives mean evaluation reward 96.
