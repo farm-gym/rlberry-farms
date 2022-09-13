7613b264526a562718473508cd9888530646723b
Timothee Mathieu <tmathieu@sequel-flanders.lille.inria.fr>
2022-09-13 17:10:51 +0200

    advanced info

---
+++ challenge/advanced_info.py
@@ -0,0 +1,39 @@
+from redis import Redis
+from rq.command import send_stop_job_command
+import sys
+from rq.job import Job
+from rq import Queue
+from rq.registry import StartedJobRegistry
+import pandas as pd
+from tabulate import tabulate
+import re
+
+redis = Redis()
+registry = StartedJobRegistry(name = "default", connection=redis)
+
+df = pd.DataFrame()
+
+for job_id in  registry.get_job_ids():
+    job = Job.fetch(job_id, connection=redis)
+    desc = job.description
+    name = re.search(r"(?<=name=')\w+", desc).group(0)
+    df = pd.concat([df, pd.DataFrame({'submitter' : [name],
+                                      "id":job_id,
+                                      'heartbeat' : [job.last_heartbeat],
+                                      'status' : [job.get_status(refresh=True)]})], ignore_index = True)
+df = df.reset_index()
+
+print(tabulate(df, headers="keys", tablefmt="psql"))
+
+print('Which job do you want to kill ? (enter its index)')
+idx = input()
+
+if (idx is not None) and (len(idx)>0):
+    job_id = df.iloc[int(idx)]['id']
+    try:
+        send_stop_job_command(redis, job_id)
+    except:
+        job = Job.fetch(job_id, connection=redis)
+        job.cancel()
+        job.delete()
+
